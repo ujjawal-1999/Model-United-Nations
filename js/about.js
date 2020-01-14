75b029e41df1eb4233b8ead21419e4bee517590d
arshita04 <arshi.narayan04@gmail.com>
2020-01-14 16:48:29 +0530

    changed

---
+++ js/about.js
@@ -0,0 +1,25 @@
+
+var i = 0;
+var j = 0;
+var txt = 'NIT Silchar';
+var txt1 = 'Model United Nations';
+var speed = 100;
+var speed1 = 100;
+ window.onload = function typeWriter() {
+   $('.text').addClass("Typed");
+  if (i < txt.length) {
+    document.getElementById("text").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+};
+setTimeout(typeWriter1,2000);
+   function typeWriter1()
+    {
+  if (j < txt1.length) {
+    document.getElementById("text1").innerHTML += txt1.charAt(j);
+    j++;
+    setTimeout(typeWriter1, speed1);
+  }
+}
+
