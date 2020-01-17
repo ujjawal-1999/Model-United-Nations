0eccc9d09523d0ebbe2698f88e2054fa94ea1a8a
Ujjawal Jain <jainujjawal1999@gmail.com>
2020-01-17 22:41:41 +0530

    Deployment

--- js/about.js
+++ js/about.js
@@ -2,7 +2,7 @@
 var i = 0;
 var j = 0;
 var txt = 'Model United Nations';
-var txt1 = '14 . 2 . 2020';
+var txt1 = '14 . 15 . 16 Feb 2020';
 var speed = 100;
 var speed1 = 100;
  window.onload = function typeWriter() {
